be15d360c66db9d5a91cb1af5ff99958c622d910
ansh3108 <anshkumar9686@gmail.com>
2025-06-04 19:48:31 +0530

    allow to list tokens

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -1,6 +1,74 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.27;
 
-contract Factory {
-    string public name="Factory";
+import { Token } from "./Token.sol";
+contract Factory{
+    uint256 public immutable fee;
+    address public owner;
+
+    uint256 public totalTokens;
+    address[] public tokens;
+    mapping(address => TokenSale) public tokenToSale;
+
+    struct TokenSale {
+        address token;
+        string name;
+        address creator;
+        uint256 sold;
+        uint256 raised;
+        bool isOpen;
+    }
+
+    event Created(address indexed token)
+
+   
+
+    constructor(uint256 _fee) {
+        fee = _fee;
+        owner = msg.sender;
+    }
+
+    function getTokenSale(uint256 _index) public view returns (TokenSale memory) {
+        return tokenToSale[tokens[_index]];
+    }
+
+
+    function create(
+        string memory _name, 
+        string memory _symbol
+        ) external payable {
+        //Make sure the fee is correct
+        require(msg.value >= fee, "Insufficient fee sent");
+
+        //Create a new token
+        Token token = new Token(msg.sender, _name, _symbol, 1_000_000 ether);
+
+        //Save the token for later use
+        tokens.push(address(token));
+        
+        totalTokens++;
+
+        emit TokenCreated(address(token), msg.sender, _name, _symbol);
+
+        // List the token for sale
+        // address token;
+        // string name;
+        // address creator;
+        // uint256 sold;
+        // uint256 raised;
+        // bool isOpen;
+        TokenSale memory sale = TokenSale(
+            address(token),
+            _name,
+            msg.sender,
+            0, //sold
+            0, //raised
+            true //isOpen1
+        );
+
+        tokenToSale[address(token)] = sale;
+
+        // Tell people it's live
+        emit Created(address(token));
+    }
 }
